56a2cc2976e62b1060e6043fb6078326541d0613
Michael <skipiano@gmail.com>
2021-06-14 13:06:44 -0700

    change module to be able to press cmd button

--- main.py
+++ main.py
@@ -5,54 +5,66 @@ import shutil
 import keyboard
 from datetime import datetime
 from PIL import Image, ImageGrab
-
+from pynput import mouse, keyboard
 saves = "/Users/hi/Library/Application Support/minecraft/saves/"
+saves0 = "/Users/hi/Desktop/MultiInstance/Minecraft0/saves/"
+saves1 = "/Users/hi/Desktop/MultiInstance/Minecraft1/saves/"
+saves2 = "/Users/hi/Desktop/MultiInstance/Minecraft2/saves/"
+saves3 = "/Users/hi/Desktop/MultiInstance/Minecraft3/saves/"
+
 delay = 0.1 
 pyautogui.PAUSE = 0
+control = Controller()
 
 def createWorld():
     for folder in os.listdir(saves):
         if folder[0] != '_':
             shutil.move(saves+folder, saves+"_oldWorlds/"+str(datetime.now()))
-    pyautogui.press('\t')
-    pyautogui.press('enter')
+    control.tap('tab')
+    control.tap('enter')
     time.sleep(delay)
-    pyautogui.press('\t')
-    pyautogui.press('\t')
-    pyautogui.press('\t')
-    pyautogui.press('enter')
+    control.tap('tab')
+    control.tap('tab')
+    control.tap('tab')
+    control.tap('enter')
     time.sleep(delay)
-    pyautogui.press('\t')
-    pyautogui.press('\t')
-    pyautogui.press('enter')
-    pyautogui.press('enter')
-    pyautogui.press('enter')
-    pyautogui.press('\t')
-    pyautogui.press('\t')
-    pyautogui.press('\t')
-    pyautogui.press('\t')
-    pyautogui.press('\t')
+    control.tap('tab')
+    control.tap('tab')
+    control.tap('enter')
+    control.tap('enter')
+    control.tap('enter')
+    control.tap('tab')
+    control.tap('tab')
+    control.tap('tab')
+    control.tap('tab')
+    control.tap('tab')
     time.sleep(delay)
-    pyautogui.press('enter')
+    control.tap('enter')
 
 def exitWorld():
-    pyautogui.press('esc')
-    pyautogui.keyDown('shift')
-    pyautogui.press('tab')
-    pyautogui.keyUp('shift')
-    pyautogui.press('enter')
+    control.tap('esc')
+    control.press('shift')
+    control.tap('tab')
+    control.release('shift')
+    control.tap('enter')
+
+
+def resetting():
+    exitWorld()
+    while True:
+        im = ImageGrab.grab(bbox = None)
+        if (im.getpixel((1334, 897)) != (113, 113, 113, 255)):
+            time.sleep(delay)
+        else:
+            break
+    createWorld()
+    control.press('cmd')
+    control.tap('tab')
+    control.tap('tab')
+    control.tap('tab')
 
 
 if __name__ == "__main__":
     print("running auto resetter...")
     print("start up a new world manually and start resetting")
-    while True:
-        keyboard.wait('shift+u')
-        exitWorld()
-        while True:
-            im = ImageGrab.grab(bbox = None)
-            if (im.getpixel((1334, 897)) != (113, 113, 113, 255)):
-                time.sleep(delay)
-            else:
-                break
-        createWorld()
+    keyboard.add_hotkey('space+u', resetting())
